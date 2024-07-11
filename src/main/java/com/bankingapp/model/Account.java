f2a92747e8cb48098141a8d30ec477264066bdec
prakashb-ai <prakashbodicse@gmail.com>
2024-07-11 23:44:27 +0530

    add

--- src/main/java/com/bankingapp/model/Account.java
+++ src/main/java/com/bankingapp/model/Account.java
@@ -1,24 +1,13 @@
 package com.bankingapp.model;
 
+import jakarta.persistence.*;
+
 import java.util.List;
-
-
-import jakarta.persistence.CascadeType;
-
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
+@Entity
 public class Account {
 
     @Id
@@ -29,10 +18,9 @@ public class Account {
 
     private Double balance;
 
-    // Other fields as needed
     @ManyToOne
-    private Applicant applicant; 
-    
+    private Applicant applicant;
+
     @OneToMany(mappedBy = "senderAccount", cascade = CascadeType.ALL)
     private List<Transaction> sentTransactions;
 
--- src/main/java/com/bankingapp/model/Transaction.java
+++ src/main/java/com/bankingapp/model/Transaction.java
@@ -1,18 +1,8 @@
 package com.bankingapp.model;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
-
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 
 @Entity
 @Data
@@ -29,8 +19,6 @@ public class Transaction {
     private LocalDateTime timestamp;
 
     private String remarks;
-
-    // Other fields as needed
 
     @ManyToOne
     @JoinColumn(name = "sender_account_id")
