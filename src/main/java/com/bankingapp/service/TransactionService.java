e5a024c35b22ef35e3172826ec87a25f339c6c64
prakashb-ai <prakashbodicse@gmail.com>
2024-07-11 22:36:47 +0530

    add

--- src/main/java/com/bankingapp/service/TransactionService.java
+++ src/main/java/com/bankingapp/service/TransactionService.java
@@ -1,24 +1,86 @@
-package com.bankingapp.service;
+package com.bankingapp.model;
 
-import java.util.List;
+import java.time.LocalDateTime;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
-import com.bankingapp.model.Transaction;
-import com.bankingapp.repository.TransactionRepository;
+@Entity
+public class Transaction {
 
-@Service
-public class TransactionService {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    @Autowired
-    private TransactionRepository transactionRepository;
+    private Double amount;
 
-    public Transaction createTransaction(Transaction transaction) {
-        return transactionRepository.save(transaction);
+    private LocalDateTime timestamp;
+
+    private String remarks;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_account_id")
+    private Account senderAccount;
+
+    @ManyToOne
+    @JoinColumn(name = "recipient_account_id")
+    private Account recipientAccount;
+
+    // Constructors
+    public Transaction() {
+        // Default constructor
     }
 
-    public List<Transaction> getTransactionsByAccountNumber(String accountNumber) {
-        return transactionRepository.findBySenderAccount_AccountNumberOrRecipientAccount_AccountNumber(accountNumber, accountNumber);
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public Account getSenderAccount() {
+        return senderAccount;
+    }
+
+    public void setSenderAccount(Account senderAccount) {
+        this.senderAccount = senderAccount;
+    }
+
+    public Account getRecipientAccount() {
+        return recipientAccount;
+    }
+
+    public void setRecipientAccount(Account recipientAccount) {
+        this.recipientAccount = recipientAccount;
     }
 }
