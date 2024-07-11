0e2ddcf1cd61cfb14d30b8904db7ee41800ef6ae
prakashb-ai <prakashbodicse@gmail.com>
2024-07-11 22:42:15 +0530

    add

--- src/main/java/com/bankingapp/service/TransactionService.java
+++ src/main/java/com/bankingapp/service/TransactionService.java
@@ -1,86 +1,16 @@
-package com.bankingapp.model;
+package com.bankingapp.repository;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 
-@Entity
-public class Transaction {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+import com.bankingapp.model.Transaction;
 
-    private Double amount;
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
-    private LocalDateTime timestamp;
+	 List<Transaction> findBySenderAccountAccountNumberOrRecipientAccountAccountNumber(String senderAccountNumber, String recipientAccountNumber);
 
-    private String remarks;
-
-    @ManyToOne
-    @JoinColumn(name = "sender_account_id")
-    private Account senderAccount;
-
-    @ManyToOne
-    @JoinColumn(name = "recipient_account_id")
-    private Account recipientAccount;
-
-    // Constructors
-    public Transaction() {
-        // Default constructor
-    }
-
-    // Getters and setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Double amount) {
-        this.amount = amount;
-    }
-
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(LocalDateTime timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public String getRemarks() {
-        return remarks;
-    }
-
-    public void setRemarks(String remarks) {
-        this.remarks = remarks;
-    }
-
-    public Account getSenderAccount() {
-        return senderAccount;
-    }
-
-    public void setSenderAccount(Account senderAccount) {
-        this.senderAccount = senderAccount;
-    }
-
-    public Account getRecipientAccount() {
-        return recipientAccount;
-    }
-
-    public void setRecipientAccount(Account recipientAccount) {
-        this.recipientAccount = recipientAccount;
-    }
 }
